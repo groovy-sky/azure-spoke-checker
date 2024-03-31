4cb50b1862e162dee3667072f34c215ebde76f25
groovy-sky <groovy-sky>
2024-03-31 23:35:33 +0300

    working on

--- parent/modules/az-rest-call/main.tf
+++ parent/modules/az-rest-call/main.tf
@@ -26,15 +26,29 @@ locals {
   res_id_split = split("/", var.resource_id)
   res_id_split_len = length(local.res_id_split)
   res_type = "${local.res_id_split[local.res_id_split_len - 3]}/${local.res_id_split[local.res_id_split_len - 2]}"
+  subscription_id = local.res_id_split[2]
 }
 
 
-resource "azapi_resource_action" "response" {
+resource "azapi_resource_action" "resource_action" {
   type = "${local.res_type}@${var.api_ver}"
   resource_id = var.resource_id
   method = var.method
 }
 
-output "response" {
-  value = local.res_type
+data "azapi_resource" "resource" {
+  type = "${local.res_type}@${var.api_ver}"
+  resource_id = var.resource_id
+  response_export_values = ["*"]
+}
+
+
+data "azapi_resource_list" "resource_list" {
+  type                   = "${local.res_type}@${var.api_ver}"
+  parent_id              = "/subscriptions/${local.subscription_id}"
+  response_export_values = ["*"]
+}
+
+output "resource_list" {
+  value = jsondecode(data.azapi_resource_list.resource_list.output)
 }
